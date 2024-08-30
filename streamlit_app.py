da6caa5b172a1c780efd03813a1e3ec00583d82c
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-08-30 18:09:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -285,22 +285,23 @@ def get_player_match_values(merged_df, player_name, match_name, col):
     return player_values
 
 def player_match_compare():
-    if 'match_data_loaded' not in st.session_state:
-        with open('league_dict.json', 'r') as f:
-            league_dict = json.load(f)
+    with open('league_dict.json', 'r') as f:
+        league_dict = json.load(f)
 
-        # Create a mapping from league names to keys
-        league_name_to_key = {v['FBref']: k for k, v in league_dict.items()}
+    # Create a mapping from league names to keys
+    league_name_to_key = {v['FBref']: k for k, v in league_dict.items()}
 
-        # User selects the league
-        league_options = list(league_name_to_key.keys())
-        selected_league_name = st.selectbox("Select the league", league_options)
+    # User selects the league
+    league_options = list(league_name_to_key.keys())
+    selected_league_name = st.selectbox("Select the league", league_options, key='match_league')
 
-        # Get the corresponding league key
-        selected_league_key = league_name_to_key[selected_league_name]
+    # Get the corresponding league key
+    selected_league_key = league_name_to_key[selected_league_name]
 
-        # User selects the season
-        season = st.selectbox("Select the season", ['2024-2025', '2023-2024', '2022-2023', '2021-2022', '2020-2021', '2019-2020', '2018-2019', '2017-2018'])
+    # User selects the season
+    season = st.selectbox("Select the season", ['2024-2025', '2023-2024', '2022-2023', '2021-2022', '2020-2021', '2019-2020', '2018-2019', '2017-2018'], key='match_season')
+
+    if 'match_data_loaded' not in st.session_state or st.session_state['selected_league_name'] != selected_league_name or st.session_state['season'] != season:
         fbref = initialize_fbref(selected_league_key, season)
         stats_list, _, _ = get_stats_lists()
         df_list = read_and_filter_stats_match_player(fbref)
@@ -309,9 +310,6 @@ def player_match_compare():
         st.session_state['merged_df'] = df_list[0]
         st.session_state['selected_league_name'] = selected_league_name
         st.session_state['season'] = season
-    else:
-        selected_league_name = st.session_state['selected_league_name']
-        season = st.session_state['season']
 
     merged_df = st.session_state['merged_df']
 
