dd596733697d433e0e8c597bceddd9c8b9c74c96
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-08-31 08:42:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -683,9 +683,6 @@ def delete_specific_files(directory, pattern):
             if os.path.isfile(file_path):
                 os.remove(file_path)
                 
-# Function to perform git pull
-def git_pull():
-    subprocess.run(['git', 'pull'])
 
 
 # Function to perform git operations
@@ -742,9 +739,6 @@ def check_and_copy_folder():
         delete_specific_files(fbrefdata_dir, 'teams')
         os.remove(flag_file)
         st.session_state['folder_copied'] = False
-
-        # Perform git pull to update the data
-        git_pull()
     
     # Zip the data folder and provide download link
     zip_name = 'data_folder.zip'
